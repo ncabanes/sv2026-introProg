821e22c2856790266100732236bf8fdf0bf49cf7
Nacho Cabanes <ncabanes@users.noreply.github.com>
2026-09-21 15:21:02 +0200

    Ejercicio 12: Repaso (división o resto)

---
+++ t02-variables/012a-repasoDivision.py
@@ -0,0 +1,5 @@
+# Repaso: división de 2 números
+
+a = int(input("dame un número "))
+b = int(input("dame otro número"))
+print("La división es", a/b)
---
+++ t02-variables/012b-repasoResto.py
@@ -0,0 +1,6 @@
+# Repaso: resto de la división de 2 números
+
+n1 = int(input("Dame un numero: "))
+n2 = int(input("Dame otro numero: "))
+resto = n1 % n2
+print("el resto de la division es", resto)
