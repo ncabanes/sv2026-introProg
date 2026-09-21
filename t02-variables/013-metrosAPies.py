402a93a993376754a8a2e2bdaa163d87a1ea27d3
Nacho Cabanes <ncabanes@users.noreply.github.com>
2026-09-21 15:22:13 +0200

    Ejercicio 13: input + float (metros a pies)

---
+++ t02-variables/013-metrosAPies.py
@@ -0,0 +1,10 @@
+# Pide la estatura en metros (quizá con decimales)
+# Conviértela a pies
+
+metros = float(input("Cuanto mides en metros? "))
+print("En pies mides", metros * 3.281)
+
+# Alternativa:
+# metros = float(input("Cuanto mides en metros? "))
+# pies = metros * 3.281
+# print("En pies mides", pies)
