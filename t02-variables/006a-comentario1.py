37a187216a26b71fb651af1887a8cd408ac38f1c
Nacho Cabanes <ncabanes@users.noreply.github.com>
2026-09-21 15:18:50 +0200

    Ejercicio 6: Comentarios

---
+++ t02-variables/006a-comentario1.py
@@ -0,0 +1,5 @@
+# Ejercicio de repaso
+# con comentario de ejemplo
+
+print( (15+5) * (15-5) )
+print( 15**2 - 5**2 )
---
+++ t02-variables/006b-comentario2.py
@@ -0,0 +1,5 @@
+# Ejercicio de repaso
+# con comentario de ejemplo
+
+print("Repaso del jueves")
+print( (15-21) ** 3 )
