90889a231de1c887b0149c3088bfdae548d50fe5
Nacho Cabanes <ncabanes@users.noreply.github.com>
2026-09-21 15:20:43 +0200

    Ejercicio 11: Fahrenheit a Celsius

---
+++ t02-variables/011-fahrenheit.py
@@ -0,0 +1,10 @@
+# Vamos a crear un conversor de
+# grados Fahrenheit a grados Celsius (centígrados).
+
+# Debes encontrar la "fórmula" que te
+# permite convertir de uno a otro y crear
+# un programa que la aplique.
+
+fahrenheit = int(input("Dime los grados Fahrenheit: "))
+celsius = (fahrenheit-32) * 5 / 9
+print("En Celsius son", celsius)
